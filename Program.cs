1b0aed4199605d6e86734b24fe3c45157f593917
jaimeload <jaime.si.load@gmail.com>
2023-01-24 13:49:17 +0530

    add program code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[4] {"Hello, World!", "=)", "24.01.2023", "Hi!"};
+string[] array2 = new string[array1.Length];
+
+FillArray2(array1, array2);
+PrintArray(array1);
+Console.WriteLine();
+PrintArray(array2);
+
+
+void FillArray2(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}   ");
+    }
+}
